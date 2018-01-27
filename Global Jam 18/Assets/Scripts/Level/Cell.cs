f59ffaf58a8edee54e20a2e43b4cf658abae7a9e
Joe Sycalik <sycalik@gmail.com>
2018-01-27 15:45:57 -0500

    Initial setup for 5x5 cell grid levels

---
+++ Global Jam 18/Assets/Scripts/Level/Cell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    public Coordinates coordinates;
+
+    public bool hasRobot;
+
+    public GameObject occupier;
+
+    private void Awake()
+    {
+        
+    }
+}
---
+++ Global Jam 18/Assets/Scripts/Level/CellGrid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellGrid : MonoBehaviour {
+
+    public int x = 5;
+    public int z = 5;
+
+    [SerializeField]
+    public Cell[] cells;
+
+    private void Awake()
+    {
+
+    }
+}

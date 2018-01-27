763f82f5f93ff03a9c55a021d9bf2dd2794e9f32
Joe Sycalik <sycalik@gmail.com>
2018-01-27 12:11:28 -0500

    GameManager, GameSoundManager, Art Folder

---
+++ Global Jam 18/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager m_instance = null;
+    public static GameManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                var go = new GameObject("GameManager");
+                m_instance = go.AddComponent<GameManager>();
+            }
+            return m_instance;
+        }
+    }
+
+
+    void Awake()
+    {
+        if (m_instance != this && m_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        m_instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    } //End Awake
+} //End class
